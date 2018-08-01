41b566842713533f78fab33ecff7744df39c01f4
Leonardo <leo.barroso.silva@gmail.com>
2018-08-01 17:40:10 -0300

    Add activity endpoint instead of task.

---
+++ spec/docusign_dtr/models/activity_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../../spec_helper'
+
+RSpec.describe DocusignDtr::Models::Activity do
+  let(:client) { double }
+  let(:activity_attrs) do
+    {
+      document_activity_id: 99,
+      action: "Added",
+      activity_date: "2018-06-27T21:10:54.843",
+      comment: "",
+      activity_source: "ra",
+      by_user: {
+        "userId": 999_999,
+        "firstName": "Mr. Chat",
+        "lastName": "Robot"
+      }
+    }
+  end
+  subject do
+    activity = DocusignDtr::Models::Activity.new(
+      activity_attrs
+    )
+    activity.client = client
+    activity
+  end
+
+  describe '#attributes' do
+    it 'has them' do
+      expect(subject).to have_attributes(
+        activity_attrs
+      )
+    end
+  end
+end
